da7ab2ac05215d709980ed5e8504e482c0b7ff76
Aaron K Redshaw <akredshaw@gmail.com>
2020-03-13 11:49:56 -0700

    Update to formula and notes

--- TensorflowMathFormula.py
+++ TensorflowMathFormula.py
@@ -4,11 +4,20 @@ from tensorflow import keras
 
 model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
 
-model.compile(optimizer='sgd', loss='mean_squared_error')
+model.compile(optimizer='sgd', loss='mse')
 
 xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float) # see formula on next line.
-ys = np.array([-2.0, 1.0, 4.0, 7.0, 10.0, 13.0], dtype=float) # using the formula y = 3x + 1.
+ys = np.array([-1.0, 0.5, 2.0, 3.5, 5.0, 6.5], dtype=float) # using the formula y = (3x + 1)/2.
+
+model.fit(xs, ys, epochs=5)
+print(model.predict([7.0])) # y = (3x + 1)/2. So, if x is 10, y should give you something near 31.
+
+model.fit(xs, ys, epochs=20)
+print(model.predict([7.0])) # y = (3x + 1)/2. So, if x is 10, y should give you something near 31.
 
 model.fit(xs, ys, epochs=500)
+print(model.predict([7.0])) # y = (3x + 1)/2. So, if x is 10, y should give you something near 31.
 
-print(model.predict([10.0])) # y = 3x + 1. So, if x is 10, y should give you something near 31.
+model.fit(xs, ys, epochs=1000)
+print(model.predict([7.0])) # y = (3x + 1)/2. So, if x is 10, y should give you something near 31.
+# Results should be approaching 11. 3x7=21, 21+1=22, 22/2=11
