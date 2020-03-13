7d62744c89c66a28a465c86127a78bd82209cc7d
Aaron K Redshaw <akredshaw@gmail.com>
2020-03-13 13:31:09 -0700

    Adding line spaces at end of iterations

--- TensorflowMathFormula.py
+++ TensorflowMathFormula.py
@@ -10,14 +10,19 @@ xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float) # see formula on nex
 ys = np.array([-1.0, 0.5, 2.0, 3.5, 5.0, 6.5], dtype=float) # using the formula y = (3x + 1)/2.
 
 model.fit(xs, ys, epochs=5)
-print(model.predict([7.0])) # y = (3x + 1)/2. So, if x is 10, y should give you something near 31.
+print(model.predict([7.0])) # Only 5 iterations. This won't be very accurate. You will see this on this page.
+print("\n")
 
 model.fit(xs, ys, epochs=20)
-print(model.predict([7.0])) # y = (3x + 1)/2. So, if x is 10, y should give you something near 31.
+print(model.predict([7.0])) # After 20 iterations, it should be better, but not really accurate. Scroll down to 20 to see this.
+print("\n")
 
 model.fit(xs, ys, epochs=500)
-print(model.predict([7.0])) # y = (3x + 1)/2. So, if x is 10, y should give you something near 31.
+print(model.predict([7.0])) # 500 iterations. Getting there. Scroll down to 500 to see this.
+print("\n")
 
 model.fit(xs, ys, epochs=1000)
-print(model.predict([7.0])) # y = (3x + 1)/2. So, if x is 10, y should give you something near 31.
-# Results should be approaching 11. 3x7=21, 21+1=22, 22/2=11
+print(model.predict([7.0])) # 1000 iterations. Very accurate. Never perfect, however. Scroll down to the bottom to see this.
+
+# y = (3x + 1)/2. So, 3x7=21, 21+1=22, 22/2=11
+# Your output should be something aproaching 11
